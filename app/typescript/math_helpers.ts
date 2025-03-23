0e1eccfce02781c9e9db71939f00c2a21ff610e2
Scapsters <scotty.happy@gmail.com>
2025-03-23 16:44:14 -0400

    minor: add prephase constant

--- app/typescript/math_helpers.ts
+++ app/typescript/math_helpers.ts
@@ -2,10 +2,11 @@ import { raw_items } from './wheel_info'
 
 const delta_t = 5
 const phase = -90
+const prephase = 90 // This affects the position of the wheel when loading
 
 // Circular equations based off of https://www.desmos.com/calculator/cvaldk77ud, created by me
 export const circular_rotate = (index: number, scroll: number): number =>
-    (mod((delta_t * index - 90 * scroll), ((raw_items.length + 1)) * delta_t)) + phase
+    (mod((delta_t * index - 90 * scroll) + prephase, ((raw_items.length + 1)) * delta_t)) + phase
 
 function mod(n: number, m: number) {
     return ((n % m) + m) % m;
