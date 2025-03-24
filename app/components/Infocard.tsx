3533f4bc26ee53322b2713ffdca73f4d6f0d4bc2
Scapsters <scotty.happy@gmail.com>
2025-03-23 23:08:37 -0400

    major: add infocard functionality its very messy.

---
+++ app/components/Infocard.tsx
@@ -0,0 +1,59 @@
+import { lexendPeta } from '@/typescript/css_constants'
+import { useEffect, useRef } from 'react'
+
+export default function Infocard() {
+    const divRefs = [useRef<HTMLDivElement>(null), useRef<HTMLDivElement>(null), useRef<HTMLDivElement>(null)]
+    const spanRefs = [useRef<HTMLElement>(null), useRef<HTMLElement>(null)]
+
+    useEffect(() => {
+        const updateStyles = (e: MouseEvent, value: string) => {
+            const anchorIndex = divRefs.findIndex((ref) => ref.current === e.target)
+            const anchor = divRefs[anchorIndex].current
+            const spans = [spanRefs[anchorIndex - 1], spanRefs[anchorIndex]]
+
+            spans.forEach((span) => {
+                if (span?.current) span.current.style.paddingTop = value
+            })
+            if (anchor) anchor.style.paddingTop = value
+        }
+
+        const mouseEnterHandler = (e: MouseEvent) => updateStyles(e, '80px')
+        const mouseExitHandler = (e: MouseEvent) => updateStyles(e, '0px')
+
+        divRefs.forEach((anchor) => {
+            anchor.current?.addEventListener('mouseenter', mouseEnterHandler)
+            anchor.current?.addEventListener('mouseleave', mouseExitHandler)
+        })
+    })
+
+    const info = (
+        <div className="p-4 text-xl flex" style={lexendPeta.style}>
+            <div ref={divRefs[0]} className="infocardfast  max-h-[25]">
+                <a href="tel:1-603-213-3404" className="infocardfast">603-213-3404</a>
+            </div>
+            &nbsp;&nbsp;
+            <span ref={spanRefs[0]} className="infocardslow">
+                &nbsp;&nbsp;<div className="border-l-2 ml-1 h-50 relative bottom-50 border-[#393939] infocardslow" ></div>
+                &nbsp;&nbsp;&nbsp;
+            </span>
+            <div ref={divRefs[1]} className="infocardfast max-h-[25]">
+                <a href="https://github.com/Scapsters" className="infocardfast">github.com/Scapsters</a>
+            </div>
+            &nbsp;&nbsp;
+            <span ref={spanRefs[1]} className="infocardslow">
+                &nbsp;&nbsp;<div className="border-l-2 ml-1 h-50 relative bottom-50 border-[#393939] infocardslow"></div>
+                &nbsp;&nbsp;&nbsp;
+            </span>
+            <div ref={divRefs[2]} className="infocardfast max-h-[25]">
+                <a href="mailto:scotty.happy@gmail.com" className="infocardfast">scotty.happy@gmail.com</a>
+            </div>
+        </div>
+    )
+
+    return (
+        <>
+            <div className="w-full absolute h-5 -z-1 bg-[#FFFAEF]"></div>
+            <div className="w-max absolute -z-2">{info}</div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import { Project } from './typescript/project_card_info'
 import Wheel from './components/Wheel'
 import { ProjectCard } from './components/ProjectCard'
+import Infocard from './components/Infocard'
 
 export default function Home() {
     const [project, setProject] = useState<Project | null>(null)
@@ -11,6 +12,7 @@ export default function Home() {
         <>
             <Wheel setProject={setProject}></Wheel>
             <ProjectCard project={project}></ProjectCard>
+			<Infocard></Infocard>
         </>
     )
 }
