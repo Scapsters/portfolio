255a7a4cbbad5868c27d0b88d720577967a199dd
Scapsters <scotty.happy@gmail.com>
2025-03-28 14:30:10 -0400

    minor: add image gallery

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import { Project } from '../typescript/project_card_info'
 import { lexendExa, lexendGiga, lexendPeta } from '../typescript/css_constants'
-import Image from 'next/image'
+import ImageGallery from 'react-image-gallery'
+import 'react-image-gallery/styles/css/image-gallery.css'
 
 /**
  * Also serves as intro card (When no projects are selected)
@@ -8,29 +9,61 @@ import Image from 'next/image'
 export function ProjectCard({ project }: Readonly<{ project: Project | null }>) {
     const common_css = 'absolute'
     return project ? (
-        <div className={`z-2 top-50 left-30 w-2/3 ${common_css}`}>
+        <div className={`z-2 top-40 left-30 w-2/3 ${common_css}`}>
+            <div className="flex">
+                {project.demo ? (
+                    <>
+                        <a className="hover:underline text-blue-500" href={project.demo}>
+                            View Live Project
+                        </a>{' '}
+                        <span className="mx-4">|</span>
+                    </>
+                ) : (
+                    <></>
+                )}
+                {project.github ? (
+                    <>
+                        <a className="hover:underline text-blue-500" href={project.github}>
+                            GitHub
+                        </a>{' '}
+                    </>
+                ) : (
+                    <></>
+                )}
+                {project.date ? (
+                    <>
+                        <p className="grow text-right justify-end">
+                            {project.date}
+                        </p>{' '}
+                    </>
+                ) : (
+                    <></>
+                )}
+            </div>
             <p style={lexendGiga.style} className="text-4xl mb-4">
                 {project.name}
             </p>
-            <div className="bg-amber-600/10 w-1/1 p-4 m-2">
+            <div className="w-1/1 p-4 bg-amber-600/10">
                 {project.description.map((description) => (
                     <p className="m-4" key={description}>
                         {description}
                     </p>
                 ))}
-            </div>
-            <div className="flex bg-amber-600/10 p-4 w-1/1 m-2">
-                <div className="bg-amber-600/10 w-2/3 m-2">
-                    {project.image ? (
-                        <Image
-                            alt="project images"
-                            src={project.image.url}
-                            width={project.image.width}
-                            height={project.image.height}
-                        />
-                    ) : null}
+                <p className="text-xl m-4"> Features </p>
+                <div>
+                    {project.features?.map((feature) => (
+                        <p className="m-2" key={feature}>
+                            {' '}
+                            - {feature}{' '}
+                        </p>
+                    ))}
                 </div>
-                <ul className="bg-amber-600/10 p-4 m-2 grow">
+            </div>
+            <div className="flex w-1/1">
+                <div className="w-2/3 p-4 mt-4 mr-2 bg-amber-600/10 ">
+                    {project.image ? <ImageGallery items={project.image} /> : null}
+                </div>
+                <ul className="grow p-4 mt-4 ml-2 bg-amber-600/10" >
                     <p className="text-3xl text-[var(--foreground)]">Tech Stack</p>
                     {project.technologies.map((technology) => (
                         <div key={technology + ' container'} className="flex text-2xl text-right items-center m-4">
@@ -39,10 +72,12 @@ export function ProjectCard({ project }: Readonly<{ project: Project | null }>) 
                             <img
                                 key={technology + ' logo'}
                                 alt={technology}
-                                src={'/logos/' + technology.replace('.', '') + '.png'}
-                                className={'h-20 aspect-auto'}
+                                src={'/logos/' + technology.replace('.', '').toLowerCase() + '.png'}
+                                className={'h-13 aspect-auto'}
                             />
-                            <li className="grow" key={technology}>{technology}</li>
+                            <li className="grow" key={technology}>
+                                {technology}
+                            </li>
                         </div>
                     ))}
                 </ul>
@@ -50,13 +85,13 @@ export function ProjectCard({ project }: Readonly<{ project: Project | null }>) 
         </div>
     ) : (
         <div className={`top-1/3 left-1/7 ${common_css}`}>
-            <p style={lexendPeta.style} className="text-6xl mb-4">
-                Welcome <span className="text-4xl">to</span>
+            <p style={lexendPeta.style} className="text-5xl mb-4">
+                Welcome <span className="text-2xl">to</span>
             </p>
-            <p style={lexendGiga.style} className="text-3xl mb-4">
+            <p style={lexendGiga.style} className="text-2xl mb-4">
                 Scott Happy&apos;s Portfolio
             </p>
-            <p style={lexendExa.style} className="text-xl mb-2">
+            <p style={lexendExa.style} className="text-l mb-2">
                 Yes, that&apos;s my name [:
             </p>
         </div>
--- app/typescript/project_card_info.ts
+++ app/typescript/project_card_info.ts
@@ -5,30 +5,46 @@ export type Project = {
     date: string
     features?: string[]
     note?: string
-    image?: { url: string, width: number; height: number }
+    image?: { original: string }[]
     github?: string
     demo?: string
 }
 
 export const projects: { [index: string]: Project } = {
     'furryslop.com': {
-        name: 'furryslop.com',
+        name: 'furryslop.com: Self-hosted Twitter Media Viewer',
         description: [
-            "Self-hosted Twitter Media Viewer. Created as an effective way to view my large catalog of liked posts. I have about 6000 liked posts on twitter, but since they are sorted by time, I'd never be able to scroll all the way to them all. But with an external web scraper, I was able to get the back-end tweet data for every post and put them in a database. Right now there's only random access, but it's not far off to be able to search by artist, etc.",
-            "Also supports copy/pasting links, the back/forward arrows, and discord embedding! (And possibly other sites). Right now it runs locally on my computer, but I'm actively porting it to AWS, and then from there I'll port it to RIT NextHop's servers. This has been a great exercise for me in web development and web infrastructure.",
+            "Created as a way to view my 6000 liked posts. Utilized a web scraper to collect back-end tweet data for every post and enter it into a database. Currently, only random images may be displayed.",
         ],
-        technologies: ['React', 'TypeScript', 'Express.js', 'PostgreSQL'],
+        features: [
+            "Prefetching and prerendering for little to no lag",
+            "Responsive UI based off user feedback",
+            "Cross-site embeds using Twitter Card API",
+            "(Partial) support for back/forward arrow navigation",
+        ],
+        technologies: ['React', 'TypeScript', 'Tailwind', 'Express.js', 'PostgreSQL' ],
         date: 'December 2024 - Present',
-        image: { url:'/furrysloppreview.png', width: 2401, height: 132 },
+        demo: 'https://furryslop.com',
+        github: 'https://github.com/Scapsters/Furry-Slop',
+        image: [ { original:'/furryslop/preview.png' }, { original: '/furryslop/componentuml.png'}, { original: '/furryslop/sequence.png'} ],
     },
     JSThread: {
         name: 'JSThread: Java-style threading in Node.js',
+        description: [
+            "Born out of curiosity, this is a recreation of Java Threads inside of a single-threaded programming language. Design overview and documentation available in the demo."
+        ],
+        features: [
+            'Leverages web-workers with advanced communication to simulate synchronized blocks, keys, joins, etc.',
+            'Built-in code editor (and color-coded output console) with examples to see it running yourself!'
+        ],
         date: 'February 2025 - February 2025',
-        description: ['flint and steel'],
+        github: 'https://github.com/Scapsters/swen_342_github',
+        demo: 'https://scapsters.github.io/swen_342_github/JSThread/dist/',
+        image: [ { original: '/jsthread/header.png' }, { original: '/jsthread/console.png' } ],
         technologies: ['Typescript', 'Node.js', 'Vite.js', 'Tailwind'],
     },
     'YoLink Center': {
-        name: 'YoLink Center: Communication with commercial IoT devices',
+        name: 'YoLink Center: Leveraging IoT API',
         date: 'November 2024 - December 2024',
         description: ['charlie oh nooooo not the poisonous fugu fish'],
         technologies: ['Python', 'Flask', 'MySQL'],
