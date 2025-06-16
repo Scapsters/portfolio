c68d09bee93ae000dd46ff879d43971e41cea6b5
Scapsters <scotty.happy@gmail.com>
2025-06-16 18:14:48 -0400

    mostly fix framerate and make it all a lot better

--- app/components/Wheel.tsx
+++ app/components/Wheel.tsx
@@ -3,7 +3,9 @@ import { circular_rotate, isVisible } from '../typescript/math_helpers'
 import { getItemColor, PortfolioData, Project, Tool} from '@/typescript/wheel_info'
 import { ProjectContext } from '@/contexts'
 
-const SCROLL_VELOCITY_FACTOR = 0.0002
+const SCROLL_VELOCITY_FACTOR = 0.00007
+const FRICTION = 0.996
+const DEFAULT_FRAME_RATE = 120
 
 function Group({
     header, items, startingIndex, itemRefs
@@ -14,13 +16,13 @@ function Group({
         <ItemWrapper ref={itemRefs.headerRef}>
             <Header text={header} toggleSection={() => setVisible(!visible)}/>
         </ItemWrapper>
-        {items.map((item, index) => {
+        {visible ? items.map((item, index) => {
             return (
                 <ItemWrapper ref={itemRefs.itemRefs[index]} key={item.key_name + index}>
                     <Item tool={item} index={startingIndex + index + 1}></Item>
                 </ItemWrapper>
             )
-        })}
+        }) : <></>}
     </>)
 }
 
@@ -113,21 +115,79 @@ function ItemWrapper({ ref, children }: Readonly<{ ref: React.RefObject<HTMLDivE
 
 export default function Wheel() {
 
-    // Changed by events. Performance impact low
     const [xOffset, setxOffset] = useState(700)
     const parentRef = useRef<HTMLDivElement>(null)
-    const circleRef = React.createRef<HTMLDivElement>()
-    useMoveWheelToXOffset(xOffset, circleRef, parentRef) // Ensure correct offsets, especially on page load. Performance impact negligible
+    const circleRef = useRef<HTMLDivElement>(null)
+    
+    // Shift items to xOffset
+    useEffect(() => {
+        const mainRefs = [parentRef, circleRef]
+        mainRefs.forEach(ref => {
+            if (!ref.current) return
+            ref.current.style.setProperty('transform', `translateX(-${xOffset}px)`)
+        })
+    }, [xOffset])
     
     const { selected, selectedIndex, scrollSinceSelection, setScrollSinceSelection } = useContext(ProjectContext)
 
-    const [frame, totalTime, velocity, deltaTime, wheelHoverRef] = usePhysics(setScrollSinceSelection, circleRef,  parentRef)
-    
-    useShiftOnProjectSelect(selected, setxOffset) // Move wheel on project selection toggle. Performance effect negligible
-    
-    const [position, setPosition] = useState(0)
-    useWheelPhysics(frame, setPosition, deltaTime, velocity)
+    useEffect(() => setxOffset(selected ? 550: 700), [selected])
 
+    // Accumulate dragging to affect physics
+    const deltaDrag = useRef(0)
+    useAccumulateDragging(parentRef, deltaDrag, setScrollSinceSelection)
+    
+    const wheelHoverRef = useRef<HTMLDivElement>(null)
+    const [isHovered, setIsHovered] = useState(false)
+    useHoverOverWheel(wheelHoverRef, setIsHovered)
+    
+    // Set scroll since selection
+    useEffect(() => {
+        const ref = wheelHoverRef.current
+        const scrollHandler = () => setScrollSinceSelection(true)
+        ref?.addEventListener("wheel", scrollHandler)
+        return () => ref?.removeEventListener("wheel", scrollHandler)
+    })
+    
+    // Count scroll per frame
+    const deltaScroll = useRef(0)
+    useEffect(() => {
+        const wheelHandler = (e: WheelEvent) => {
+            if (isHovered) deltaScroll.current += e.deltaY
+        }
+        const element = wheelHoverRef.current
+        element?.addEventListener('wheel', wheelHandler)
+        return () => element?.removeEventListener('wheel', wheelHandler)
+    }, [isHovered])
+
+    // Frame data
+    const framePeriod = useRef(1000 / DEFAULT_FRAME_RATE)
+    const lag = useRef(0)
+    const velocity = useRef(0)
+    const deltaTime = useRef(framePeriod.current)
+    const position = useRef(0)
+
+    // Physics loop
+    if (!scrollSinceSelection) { // Move wheel to selected item
+        const current_angle = circular_rotate(selectedIndex ?? 0, position.current)
+        const target_angle = circular_rotate(0, 0)
+        const delta_angle = current_angle - target_angle - 20 // 20 is the magic number, 
+        velocity.current = velocity.current 
+            + Math.pow(Math.tanh(delta_angle), 3) // Main function
+            * 0.0004 // Scale for funsies 
+            * ((deltaTime.current + lag.current)/ 6) // Scale for scaries
+    }
+    
+    velocity.current *= FRICTION ** (deltaTime.current + lag.current) // Friction
+    
+    velocity.current += deltaScroll.current * SCROLL_VELOCITY_FACTOR // Scrolling
+    deltaScroll.current = 0
+
+    velocity.current += deltaDrag.current * SCROLL_VELOCITY_FACTOR // Dragging
+    deltaDrag.current = 0
+
+    position.current += velocity.current * (deltaTime.current + lag.current) / 16
+    
+    // Organized per section with header, main items, and a gap between
     const itemRefs = useRef(
         Object.entries(PortfolioData).map((entry) => {
             const numItems = Object.values(entry[1]).length
@@ -139,16 +199,100 @@ export default function Wheel() {
         })
     )
 
+    // Just flattened the original array
     const flatItemRefs = useMemo(() => 
         itemRefs.current.flatMap(item => [item.headerRef, ...item.itemRefs, item.blankRef]), 
         [itemRefs]
     )
 
-    useUpdateItemRotations(flatItemRefs, position)
-    useModifyItemAnimationsWhileLoading(flatItemRefs, position, totalTime)
-    useUpdateVisibility(flatItemRefs, position)
+    // Update rotations
+    for (let i = 0; i < flatItemRefs.length; i++) {
+        const item = flatItemRefs[i].current
+        if (!item) continue
+        item.style.transform = `rotate(
+                ${-circular_rotate(i, position.current)}deg
+        )`
+    }
 
-    usePushWheelToSelectedProject(position, selectedIndex, scrollSinceSelection, velocity, deltaTime)
+    const totalTime = useRef(0)
+
+    // For some reason
+    if (totalTime.current > 1200) {
+        circleRef.current?.style.setProperty('transition', 'transform .5s ease-in-out')
+        parentRef.current?.style.setProperty('transition', 'transform .5s ease-in-out')
+    }
+
+    // Remove transitions to allow wheel movement to work
+    if (totalTime?.current ?? 0 > 1200) flatItemRefs.forEach(itemRef => itemRef?.current?.style.setProperty('transition', 'none'))
+
+    // Update item visibilities
+    flatItemRefs.forEach((itemRef, index) => {
+        if (!isVisible(index, position.current)){
+            itemRef.current?.style.setProperty('z-index', '-1')
+            itemRef.current?.style.setProperty('opacity', '0')
+        }
+        else {
+            itemRef.current?.style.setProperty('z-index', '1')
+            itemRef.current?.style.setProperty('opacity', '1')
+        }
+    })
+
+    const [frame, setFrame] = useState(0)
+    const lastRender = useRef<number>(performance.now())
+    useEffect(() => {
+        let animationId: number | null = null
+        let running = true
+
+        let accumulator = 0
+
+        let timeSinceAnimationFrame = 0
+        let timeOfLastAnimationFrame = 0
+        let trueTotalTime = 0
+
+        let hasSetTime = false
+        const frameLoop = (timestamp: number) => {
+            timeSinceAnimationFrame = timestamp - timeOfLastAnimationFrame
+            console.log(timeSinceAnimationFrame)
+            trueTotalTime += timeSinceAnimationFrame
+            if (!running) return
+
+            accumulator += timeSinceAnimationFrame
+            if (accumulator > framePeriod.current) {
+                setFrame(f => f + 1)
+                totalTime.current += framePeriod.current
+                lag.current = timestamp - lastRender.current - deltaTime.current 
+                lastRender.current = timestamp
+                
+                //lag.current = accumulator - framePeriod.current
+                //console.log("lag:" + lag.current)
+                accumulator -= framePeriod.current
+            }
+            
+            timeOfLastAnimationFrame = timestamp
+
+            animationId = requestAnimationFrame(frameLoop)
+        }
+
+        const tabOutHandler = () => {
+            if (document.hidden) {
+                running = false
+                if (animationId) cancelAnimationFrame(animationId)
+            } else {
+                running = true
+                //lastRender.current = performance.now() // Reset timestamp to avoid time jumps
+                requestAnimationFrame(frameLoop)
+            }
+        }
+
+        animationId = requestAnimationFrame(frameLoop)
+        document.addEventListener('visibilitychange', tabOutHandler)
+
+        return () => {
+            if (animationId) cancelAnimationFrame(animationId)
+            document.removeEventListener('visibilitychange', tabOutHandler)
+        }
+
+    }, [])
 
     const items = useMemo(() => {
         let globalIndex = 0
@@ -192,55 +336,6 @@ export default function Wheel() {
     </>)
 }
 
-function usePhysics(
-        setScrollSinceSelection: React.Dispatch<React.SetStateAction<boolean>>,
-        circleRef: React.RefObject<HTMLDivElement | null>,
-        parentRef: React.RefObject<HTMLDivElement | null>,
-    ): [number, React.RefObject<number>, React.RefObject<number>, React.RefObject<number>, React.RefObject<HTMLDivElement | null>] {
-        
-        const velocity = useRef(0)
-        const deltaTime = useRef(0)
-        const wheelHoverRef = useWheelInteraction(velocity, deltaTime, setScrollSinceSelection)         // Handles wheel-specific interaction. Performance impact low
-        
-        const totalTime = useRef(0)
-        const lastRenderTime = useRef(0)
-
-        const frame = useAnimationFrames(deltaTime, lastRenderTime, totalTime)    // Re-render every frame (for physics) and limit to 60 FPS. Performance impact high
-        useModifyAnimationsWhileLoading(frame, totalTime, circleRef, parentRef)   // Change transitions after loading. Performance impact minimal
-
-        return [frame, totalTime, velocity, deltaTime, wheelHoverRef]
-    }
-
-function useWheelInteraction(
-        velocity: React.RefObject<number>,
-        deltaTime: React.RefObject<number>,
-        setScrollSinceSelection: React.Dispatch<React.SetStateAction<boolean>>,
-    ) {
-        const wheelHoverRef = useRef<HTMLDivElement>(null)
-        const [isHovered, setIsHovered] = useState(false)
-
-        useImpartVelocityOnScroll(wheelHoverRef, isHovered, velocity, deltaTime, setScrollSinceSelection)   // Track mousewheel events and add velocity. Changes are reflected in the frame loop. Performance impact negligible
-        useDragToSpin(wheelHoverRef, velocity, setScrollSinceSelection)                                     // Allows wheel to be dragged. Performance impact low
-        useHoverOverWheel(wheelHoverRef, setIsHovered)                                                      // Sets the wheels to their current xOffset. Performance impact negligible
-
-        return wheelHoverRef
-    }
-
-function useUpdateVisibility(itemRefs: React.RefObject<HTMLDivElement | null>[], position: number) {
-    useEffect(() => {
-        itemRefs.forEach((itemRef, index) => {
-            if (!isVisible(index, position)){
-                itemRef.current?.style.setProperty('z-index', '-1')
-                itemRef.current?.style.setProperty('opacity', '0')
-            }
-            else {
-                itemRef.current?.style.setProperty('z-index', '1')
-                itemRef.current?.style.setProperty('opacity', '1')
-            }
-        })
-    }, [itemRefs, position])
-}
-
 function useBoldSelected(itemRef: React.RefObject<HTMLButtonElement | null>, selected: Project | Tool | null | undefined) {
     useEffect(() => {
         const element = itemRef?.current
@@ -256,29 +351,6 @@ function useBoldSelected(itemRef: React.RefObject<HTMLButtonElement | null>, sel
         }
     }, [itemRef, selected])
 }
-
-function useShiftOnProjectSelect(selected: Project | Tool | null | undefined, setxOffset: React.Dispatch<React.SetStateAction<number>>) {
-    useEffect(() => {
-        const handleProjectSelected = () => setxOffset(550)
-        const handleProjectDeselected = () => setxOffset(700)
-        if (selected) handleProjectSelected()
-        else handleProjectDeselected()
-    }, [selected, setxOffset])
-}
-
-function useUpdateItemRotations(itemRefs: React.RefObject<HTMLDivElement | null>[], position: number) {
-    useEffect(() => {
-        for (let i = 0; i < itemRefs.length; i++) {
-            const item = itemRefs[i].current
-            if (!item) continue
-            item.style.transform = `rotate(
-                 ${-circular_rotate(i, position)}deg
-            )`
-        }
-    }, [itemRefs, position])
-}
-
-
 
 function useHoverOverWheel(
     wheelHoverRef: React.RefObject<HTMLDivElement | null>,
@@ -298,124 +370,9 @@ function useHoverOverWheel(
     }, [setIsHovered, wheelHoverRef])
 }
 
-function useAnimationFrames(
-    delta_time: React.RefObject<number>,
-    lastRendertime: React.RefObject<number>,
-    totalTime: React.RefObject<number>,
-) {
-    const [frame, setFrame] = useState(0)
-
-    useEffect(() => {
-        let animationId: number | null = null
-        let running = true // Track if animation is active
-
-        let accumulator = 0;
-        const FIXED_STEP = 1000 / 60;
-
-        const updateFrame = (timestamp: number) => {
-            if (!running) return;
-
-            const delta = timestamp - lastRendertime.current;
-            accumulator += delta;
-
-            while (accumulator >= FIXED_STEP) {
-                delta_time.current = FIXED_STEP;
-                totalTime.current += FIXED_STEP;
-                lastRendertime.current = timestamp;
-                console.log(delta_time.current)
-
-                setFrame(prev => prev + 1);
-                accumulator -= FIXED_STEP;
-            }
-
-            animationId = requestAnimationFrame(updateFrame);
-        };
-
-        const tabOutHandler = () => {
-            if (document.hidden) {
-                running = false
-                if (animationId) cancelAnimationFrame(animationId)
-            } else {
-                running = true
-                lastRendertime.current = performance.now() // Reset timestamp to avoid time jumps
-                requestAnimationFrame(updateFrame)
-            }
-        }
-
-        lastRendertime.current = performance.now()
-        animationId = requestAnimationFrame(updateFrame)
-        document.addEventListener('visibilitychange', tabOutHandler)
-
-        return () => {
-            if (animationId) cancelAnimationFrame(animationId)
-            document.removeEventListener('visibilitychange', tabOutHandler)
-        }
-    }, [delta_time, lastRendertime, setFrame, totalTime])
-
-    return frame
-}
-
-function useWheelPhysics(
-    frame: number,
-    setPosition: React.Dispatch<React.SetStateAction<number>>,
-    deltaTime?: React.RefObject<number>,
-    velocity?: React.RefObject<number>,
-) {
-    useEffect(() => {
-        if(!deltaTime || !velocity) return
-        const friction = 0.03
-        velocity.current = (velocity.current * friction) * (deltaTime.current)
-        setPosition((p) => p + velocity.current * deltaTime.current / 10)
-    }, [deltaTime, frame, setPosition, velocity])
-}
-
-function useImpartVelocityOnScroll(
+function useAccumulateDragging(
     wheelHoverRef: React.RefObject<HTMLDivElement | null>,
-    isHovered: boolean,
-    velocity: React.RefObject<number>,
-    deltaTime: React.RefObject<number>,
-    setScrollSinceSelection: React.Dispatch<React.SetStateAction<boolean>>,
-) {
-    useEffect(() => {
-        
-        const wheelHandler = (e: WheelEvent) => {
-            if (!isHovered || !element) 
-                return
-
-            velocity.current += +e.deltaY * SCROLL_VELOCITY_FACTOR / (deltaTime.current / 33)
-            setScrollSinceSelection(true)
-        }
-
-        const element = wheelHoverRef.current
-        element?.addEventListener('wheel', wheelHandler)
-        return () => {
-            element?.removeEventListener('wheel', wheelHandler)
-        }
-    }, [wheelHoverRef, isHovered, velocity, setScrollSinceSelection, deltaTime])
-}
-
-function usePushWheelToSelectedProject(
-    position: number,
-    selected_index: number | null,
-    scrollSinceSelection: boolean,
-    velocity?: React.RefObject<number>,
-    deltaTime?: React.RefObject<number>,
-) {
-    useEffect(() => {
-        if (!selected_index || !velocity || !deltaTime) return
-        if (scrollSinceSelection) return
-
-        const current_angle = circular_rotate(selected_index, position)
-        const target_angle = circular_rotate(0, 0)
-
-        const delta_angle = current_angle - target_angle - 20 // 20 is the magic number, 
-        velocity.current = velocity.current + Math.pow(Math.tanh(delta_angle), 3) * 0.0004 * (deltaTime.current / 4)
-    }, [position, selected_index, scrollSinceSelection, velocity, deltaTime])
-}
-
-function useDragToSpin(
-    wheelHoverRef: React.RefObject<HTMLDivElement | null>,
-    velocity: React.RefObject<number>,
+    deltaDrag: React.RefObject<number>,
     setScrollSinceSelection: React.Dispatch<React.SetStateAction<boolean>>,
 ) {
     const isDragging = useRef(false)
@@ -434,10 +391,11 @@ function useDragToSpin(
             const clientY = 'touches' in e ? e.touches[0].clientY : e.clientY
             const deltaY = clientY - lastDragY.current
             lastDragY.current = clientY
-            if (!isDragging.current) return
-
-            velocity.current -= deltaY * SCROLL_VELOCITY_FACTOR * 0.4 // Adjust multiplier for sensitivity
-            setScrollSinceSelection(true)
+            
+            if (isDragging.current) {   
+                deltaDrag.current -= deltaY
+                setScrollSinceSelection(true)
+            }
         }
 
         const onMouseUp = () => {
@@ -458,46 +416,6 @@ function useDragToSpin(
             window.removeEventListener('mousemove', onMouseMove)
             window.removeEventListener('mouseup', onMouseUp)
         }
-    }, [wheelHoverRef, velocity, setScrollSinceSelection])
+    }, [deltaDrag, setScrollSinceSelection, wheelHoverRef])
 }
 
-function useModifyAnimationsWhileLoading(
-    frame: number,
-    totalTime: React.RefObject<number>,
-    circleRef: React.RefObject<HTMLDivElement | null>,
-    parentRef: React.RefObject<HTMLDivElement | null>,
-) {
-    useEffect(() => {
-        if (totalTime.current > 1200) {
-            circleRef.current?.style.setProperty('transition', 'transform .5s ease-in-out')
-            parentRef.current?.style.setProperty('transition', 'transform .5s ease-in-out')
-        }
-    }, [circleRef, parentRef, frame, totalTime])
-}
-
-function useModifyItemAnimationsWhileLoading(
-    itemRefs: React.RefObject<HTMLDivElement | null>[],
-    position: number,
-    totalTime: React.RefObject<number>
-) {
-    useEffect(() => {
-        if (totalTime?.current ?? 0 > 1200) 
-            itemRefs.forEach(itemRef => itemRef?.current?.style.setProperty('transition', 'none')
-    )}, [itemRefs, position, totalTime])
-}
-
-function useMoveWheelToXOffset(
-    xOffset: number,
-    circleRef: React.RefObject<HTMLDivElement | null>,
-    parentRef: React.RefObject<HTMLDivElement | null>,
-) {
-    useEffect(() => {
-        if (!circleRef.current) return
-        circleRef.current.style.setProperty('transform', `translateX(-${xOffset}px)`)
-    }, [xOffset, circleRef])
-
-    useEffect(() => {
-        if (!parentRef.current) return
-        parentRef.current.style.setProperty('transform', `translateX(-${xOffset}px)`)
-    }, [xOffset, parentRef])
-}
