d728b9dccbe4397617edd77054197c9bfc7ae705
Scott Happy <sdhhelp@rit.edu>
2025-10-28 15:11:48 -0400

    add divider between posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -17,17 +17,25 @@ export default function BlogHome() {
             flex justify-center pt-20 h-full
             dark:bg-foreground dark:text-background
         ">
-            <div className="flex flex-col w-fit items-center gap-10">
-                <p className="text-3xl">Blog Posts</p>
-                {postInfo.map(info =>
-                    <Link
-                        href={"blog/" + info.link}
-                        key={info.name}
-                        className="w-full text-left"
-                    >
-                        <p className="text-xl">{info.name}</p>
-                        <p>{info.date}</p>
-                    </Link>
+            <div className="flex flex-col w-fit items-center">
+                <p className="text-3xl pb-20">Blog Posts</p>
+                {postInfo.map((info, index) =>
+                    <>
+                        <Link
+                            href={"blog/" + info.link}
+                            key={info.name}
+                            className="
+                                w-full px-5 py-10
+                                text-left
+                                hover:bg-black/10 dark:hover:bg-white/10
+                            ">
+                            <p className="text-xl">{info.name}</p>
+                            <p>{info.date}</p>
+                        </Link>
+                        {index != postInfo.length - 1
+                            && <div className="w-full h-[2px] bg-foreground dark:bg-background" />
+                        }
+                    </>
                 )}
             </div>
         </div>
