c8dcd59fba0da05d3840e55612d35a859d7ea581
Scapsters <scotty.happy@gmail.com>
2025-11-04 15:47:07 -0500

    add back button, refine spacing

--- app/blog/[postName]/page.tsx
+++ app/blog/[postName]/page.tsx
@@ -1,5 +1,7 @@
 import Markdown from 'react-markdown'
 import { getPostContent, getPostShortFilenames } from "../file_utils"
+import Link from 'next/link'
+import { BiChevronLeft } from 'react-icons/bi'
 
 export async function generateStaticParams() {
     return getPostShortFilenames().map(filename => ({ slug: filename }))
@@ -10,23 +12,35 @@ export default async function Post({ params }:
 ) {
     const { postName } = await params
     return (
-        <div className="flex">
-            <div className="grow">fe</div>
-            <div className="grow">fe</div>
+        <div className="flex dark:bg-foreground h-full">
+            <div className="grow">
+                <div className="w-full flex justify-end mt-20">
+                    <Link 
+                        className="
+                            w-10 h-10 rounded-full 
+                            flex items-center justify-center
+                            hover:dark:bg-white/10 hover:bg-black/10
+                        "    
+                        href="/blog"
+                    >
+                        <BiChevronLeft size={32} className="text-foreground dark:text-background"></BiChevronLeft>
+                    </Link>
+                </div>
+            </div>
             <div className="
-            w-screen h-full pt-20
-            flex justify-center
-            dark:bg-foreground
+                grow pt-20
+                flex justify-center
             ">
                 <article className="
-                prose
-                text-background dark:text-foreground
-                prose-li:marker:text-foreground prose-li:marker:dark:text-background
-                dark:prose-invert
+                    prose
+                    text-foreground dark:text-background
+                    prose-li:marker:text-foreground prose-li:marker:dark:text-background
+                    dark:prose-invert
                 ">
                     <Markdown>{getPostContent(postName)}</Markdown>
                 </article>
             </div>
+            <div className="grow"></div>
         </div>
     )
 }
