09b124a9b530a55f549fbb865893d53c6b0b8a69
Scapsters <scotty.happy@gmail.com>
2025-11-04 16:43:03 -0500

    improve scrolling

--- app/blog/[postName]/page.tsx
+++ app/blog/[postName]/page.tsx
@@ -11,36 +11,53 @@ export default async function Post({ params }:
     { params: Promise<{ postName: string }> }
 ) {
     const { postName } = await params
-    return (
-        <div className="flex dark:bg-foreground h-full">
+    return (<>
+        <div className="flex h-full bg-white overflow-y-scroll pb-20">
             <div className="grow">
                 <div className="w-full flex justify-end mt-20">
                     <Link 
                         className="
+                            fixed
                             w-10 h-10 rounded-full 
                             flex items-center justify-center
-                            hover:dark:bg-white/10 hover:bg-black/10
+                            hover:bg-black/10
                         "    
                         href="/blog"
                     >
-                        <BiChevronLeft size={32} className="text-foreground dark:text-background"></BiChevronLeft>
+                        <BiChevronLeft size={32} className="text-black"></BiChevronLeft>
                     </Link>
                 </div>
             </div>
             <div className="
-                grow pt-20
-                flex justify-center
+                grow
+                flex flex-col items-center
             ">
                 <article className="
+                    py-20
                     prose
-                    text-foreground dark:text-background
-                    prose-li:marker:text-foreground prose-li:marker:dark:text-background
+                    h-fit
+                    text-black/80
+                    prose-headings:text-black
+                    prose-li:marker:text-black
                     dark:prose-invert
                 ">
                     <Markdown>{getPostContent(postName)}</Markdown>
                 </article>
+                <div className="pb-20">
+                    <Link 
+                        className="
+                            px-2 w-max h-10 
+                            flex items-center justify-center
+                            bg-foreground hover:bg-foreground/70
+                            text-white
+                        "    
+                        href="/blog"
+                    >
+                        Back to Blog
+                    </Link>
+                </div>
             </div>
             <div className="grow"></div>
         </div>
-    )
+    </>)
 }
